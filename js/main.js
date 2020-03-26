dc146309b882a17788aa49b1ad62bdb765389186
danial9696 <dan.karshenas@gmail.com>
2020-03-26 20:06:40 +0430

    aboutUs

--- js/main.js
+++ js/main.js
@@ -2,36 +2,67 @@
 
 $(document).ready(() => {
 
+    // DOM Cache
     const collapseBtn = $('#collapseBtn');
     const menu = $('.slick-menu');
+    const signUp = $('#sign-up-btn')
+    const signIn = $('#sign-in-btn')
+    const close = $('#close')
     const accordionPanel = $('.accordion-panel');
     const courseTitle = $('#course-title');
     const description = $('#description');
     const fullDescription = $('.full-description');
-    // const panel = $('.panel');
     let i;
 
     // Responsive Menu
     collapseBtn.on('click', () => {
-        // if (menu.hasClass() = 'd-none') {
-        //     menu.clas
-        // }
-        menu.toggleClass('state-btn',1000);
+        menu.toggleClass('state-btn');
     });
 
-    
+    // Responsive Menu Mega Menu
+    $('.mobile-mega-menu').on('click', function() {
+        let mobile = $(this);
+        mobile.siblings('.mega-menu').toggle();
+    });
 
-    // Accordion
+    // Close PopUp
+    close.on('click', function() {
+        $('#registerPopup').toggle(500);
+        $('#overlay').css({
+            visibility: 'hidden',
+            opacity: '0'
+        });
+    })
+
+    // Register
+    signUp.on('click', function() {
+        $('#overlay').css({
+            visibility: 'visible',
+            opacity: '1'
+        });
+        $('#registerPopup').toggle(500);
+    });
+
+    // Login
+    signIn.on('click', function() {
+        $('#overlay').css({
+            visibility: 'visible',
+            opacity: '1'
+        });
+        $('#loginPopup').toggle(500);
+    });
+
+    // Top btn for change beetwen descriptio and courser title
     accordionPanel.on('click', function()  {
         let acc = $(this);
         if($(acc).children('.panel').length === 1) {
             $(acc).children('.panel').css('border-bottom', 'none');
         }
-        $(acc).children('.panel').toggleClass('active', 500, 'easeOutSine');
+        $(acc).children('.panel').toggleClass('active', 300, 'linear');
     
     });
 
-    // Accordion on click
+     // Top btn for change beetwen descriptio and courser title
     courseTitle.on('click', function() {
       // fullDescription.css('display', 'none');
       fullDescription.toggle(500);
@@ -41,8 +72,6 @@ $(document).ready(() => {
     description.on('click', function() {
       fullDescription.toggle(500);
     });
-
-
 
     // number count for stats, using jQuery animate
     $('.counting').each(function() {
@@ -66,54 +95,25 @@ $(document).ready(() => {
         }
     
         });  
-        
-    
     });
 
-    
-
-    // $('.owl-carousel').owlCarousel({
-        
-        
-        
-        // margin: 20,
-   /*
-  animateOut: 'fadeOut',
-  animateIn: 'fadeIn',
-  */
-        // responsiveClass: true,
-        // nav: true,
-
-        // responsive: {
-        //   0: {
-        //     items: 1
-        //   },
-
-        //   600: {
-        //     items: 3
-        //   },
-
-        //   1024: {
-        //     items: 4
-        //   },
-
-        //   1366: {
-        //     items: 4,
-        //     nav:true,
-        //     loop:true,
-        //     autoplay: true,
-        //   }
-        // }
-        
+    // Teachers Description Tob Btn Changes
+    // $('.top-btn-description').children().on('click', function() {
+    //     let topBtn = $(this);
+    //     topBtn.css
     // });
 
-    // $('.similar-product').slick({
-    //   infinite: true,
-    //   slidesToShow: 3,
-    //   slidesToScroll: 3
-    // });
-    
+    $('.web-teacher').on('click', function() {
+        $('.network-teacher-inn').hide();
+        $('.web-teacher-inn').toggle();
+    });
 
+    $('.network-teacher').on('click', function() {
+        $('.web-teacher-inn').hide(); 
+        $('.network-teacher-inn').toggle();
+    });
+
+    // Carousel
     let swiper = new Swiper('.swiper-container', {
         loop: true,
         
